150de6c476236d60c95229e761ba21abdbd54b9f
James David  Mervyn Speed <speed@vm-srv-franklin.win.ntnu.no>
2019-01-21 13:39:39 +0100

    Trying some cluster analysis based on species

--- ArcticBorealHerbivoreDiversity.R
+++ ArcticBorealHerbivoreDiversity.R
@@ -7,7 +7,7 @@ require(sp)#Spatial
 require(rasterVis)#Spatial
 require(picante)#Diversity analysis
 require(vegan)#Diversity analysis
-
+require(ape)#View dendrograms
 
 # Set up ------------------------------------------------------------------
 
@@ -59,8 +59,66 @@ herbivore_dataset
 #
 #
 #Here we need to remove unused species, and add those with missing data!!!
-#
-#
+#Not complete!!
+
+spplist<-read.csv('TraitSpreadSheetJan2019.csv')
+spplist$Binomial
+
+#Match names
+#Replace dot with space to match with raster stack names
+spplist1<-spplist$Binomial
+spplist1<-sub(' ','.',spplist1)
+spplist1%in%names(herbivore_dataset)
+
+#These species are ok
+spplist1[which(spplist1%in%names(herbivore_dataset))]
+spplist1[which(spplist1%in%names(herbivore_dataset)==F)]#These species are not in the spatial data 
+names(herbivore_dataset)[which(names(herbivore_dataset)%in%spplist1==F)]#These species are in the spatial data but not spp list
+
+#Several of these are synonyms
+#Anser==Chen
+#Anas==Mareca
+#Urocitellus.parryii==Spermophilus.parryii
+
+#Remove livestock from spatial data
+livestocklist<-c('Bos.taurus','Capra.aegagrus','Ovis.aries')
+herbivore_dataset<-herbivore_dataset[[which(names(herbivore_dataset)%in%livestocklist==F)]]
+
+#Two species in spatial dataset not on trait list
+#Aythya.collaris
+#Ursus.arctos  
+
+#Species on trait list, not in spatial data - check these
+#Aix.galericulata
+#Allactaga.major
+#Allactaga.sibirica
+#Allocricetulus.eversmanni
+#Anas.platyrhynchos
+#Anas.rubripes"            
+#Cervus.canadensis #Should seperate this from Cervus elaphys in distribiton data #Also join Alces alces and A. americanus
+# Glaucomys.sabrinus   
+# Melanitta.americana 
+# Melanitta.deglandi  
+# Melanitta.stejnegeri
+# Micromys.minutus
+# Microtus.hyperboreus  
+# Napaeozapus.insignis      
+# Phasianus.colchicus     
+# Phodopus.campbelli
+# Phodopus.sungorus
+# Pteromys.volans
+# Sciurus.carolinensis
+# Sciurus.vulgaris
+# Spatula.querquedula
+# Tamias.striatus
+
+
+#Not complete!! - check the above
+
+
+
+# Species diversity analysis ----------------------------------------------
+
 
 #Diversity analysis
 #Calculate species richness 
@@ -68,5 +126,34 @@ sr<-calc(herbivore_dataset,fun=sum,na.rm=T)
 #Cells with sr>0
 speciesrichness<-sr
 speciesrichness[sr==0]<-NA
-levelplot(speciesrichness,par.settings=YlOrRdTheme,margin=F)+
+levelplot(speciesrichness,par.settings=YlOrRdTheme,margin=F)#+
   layer(sp.polygons(noreco_shppp))#Need to get better outline map at somepoint
+  
+
+#
+
+# Species based cluster analysis ------------------------------------------
+
+#Community data
+herbcomdata<-getValues(herbivore_dataset)
+herbcomdata1<-herbcomdata[rowSums(herbcomdata,na.rm=T)>1,]
+herbcomdata1[is.na(herbcomdata1)]<-0
+#Distance matrix
+dist1<-vegdist(herbcomdata1)#Bray curtis
+#Hierarchical clustering - Ward.D method
+hc1<-hclust(dist1,method='ward.D')
+plot(hc1)
+
+#Cut tree (check optimal number of clusters first)
+clusters<-cutree(hc1,k=8)
+
+#Make a raster layer for cluster and populate it 
+speciesclusts<-speciesrichness
+speciesclusts[speciesrichness>1]<-clusters
+
+mycol<-brewer.pal(8,'Dark2')
+levelplot(speciesclusts,margin=F,scales=list(draw=FALSE),col.regions=mycol,colorkey=list(at=seq(0.5,8.5,by=1)))+
+  layer(sp.polygons(noreco_shppp))#Need to get better outline map at somepoint
+
+#hc1$labels<-rep('llllllllllllllll',times=length(hc1$height)) #Bodge a coloured bar as a label
+#plot(as.phylo(hc1),tip.color=mycol[clusters],cex=0.5,no.margin=T)
